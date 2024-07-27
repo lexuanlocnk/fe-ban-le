3b08b691603ed66f21af247ea00a5272d7d16954
Vuong Hoai An <anvh@chinhnhanvn.com>
2024-07-27 15:54:52 +0700

    commit code

--- src/app/contentHomePage.jsx
+++ src/app/contentHomePage.jsx
@@ -52,64 +52,69 @@ const ContentHomePage = async ({ searchParams }) => {
   const category = searchParams["category"] ?? "laptop";
 
   return (
-    <div className="box-container-content-homepage">
-      <div className="row container-content-homepage">
-        <div className="col-12 box_container_banner   mt-2">
-          <div className="container_banner bg-white">
-            <BannerHomePage dataCategory={dataCategory} />
-          </div>
+    <>
+      <div className="col-12 box_container_banner   ">
+        <div className="container_banner bg-white">
+          <BannerHomePage dataCategory={dataCategory} />
         </div>
-        <div className="col-12 box_container_slider_product my-2">
-          <div className=" bg-white  box-slider-product ">
-            <TopProducts session={session} />
-
-            {/* <Slider3dHotProducts session={session} /> */}
-          </div>
-        </div>
-        <div className="col-12 box_container_flash_sale_bg mb-2">
-          <div className="flash-sale-bg  bg-white py-2">
-            <ProductsFlashSale />
-            {/* err */}
-          </div>
-        </div>
-        <div className="col-12  box_container_secondary_banner mb-2">
-          <div className="secondary_banner  bg-white py-2">
-            <SecondaryBanner />
-          </div>
-        </div>
-        {/*  */}
-
-        {productCategories &&
-          productCategories.length > 0 &&
-          productCategories.map((item, index) => (
-            <div key={index} className="col-12 box_container_hot_category mb-2">
-              <div
-                style={{
-                  backgroundSize: "cover",
-
-                  backgroundImage: `url(${hostImage + item.background})`,
-                }}
-                className={`${
-                  item.background ? "" : "bg-white"
-                } hot_category py-2`}
-              >
-                <FeaturedProductsCategory
-                  item={item}
-                  session={session}
-                  category={category}
-                />
-              </div>
-            </div>
-          ))}
-        <div className="col-12  box_container_hot_products mb-2">
-          <div className="hot-products  bg-white py-2">
-            <HotProducts page={page} session={session} />
-          </div>
-        </div>
-        {/*  */}
       </div>
-      <BoxModalCompare session={session} />
-    </div>
+      <div className="box-container-content-homepage">
+        <div className="row container-content-homepage">
+          <div className="col-12 box_container_slider_product my-2">
+            <div className=" bg-white  box-slider-product ">
+              <TopProducts session={session} />
+
+              {/* <Slider3dHotProducts session={session} /> */}
+            </div>
+          </div>
+          <div className="col-12 box_container_flash_sale_bg mb-2">
+            <div className="flash-sale-bg  bg-white py-2">
+              <ProductsFlashSale />
+              {/* err */}
+            </div>
+          </div>
+          <div className="col-12  box_container_secondary_banner mb-2">
+            <div className="secondary_banner  bg-white py-2">
+              <SecondaryBanner />
+            </div>
+          </div>
+          {/*  */}
+
+          {productCategories &&
+            productCategories.length > 0 &&
+            productCategories.map((item, index) => (
+              <div
+                key={index}
+                className="col-12 box_container_hot_category mb-2"
+              >
+                <div
+                  style={{
+                    backgroundSize: "cover",
+
+                    backgroundImage: `url(${hostImage + item.background})`,
+                  }}
+                  className={`${
+                    item.background ? "" : "bg-white"
+                  } hot_category py-2`}
+                >
+                  <FeaturedProductsCategory
+                    item={item}
+                    session={session}
+                    category={category}
+                  />
+                </div>
+              </div>
+            ))}
+          <div className="col-12  box_container_hot_products mb-2">
+            <div className="hot-products  bg-white py-2">
+              <HotProducts page={page} session={session} />
+            </div>
+          </div>
+          {/*  */}
+        </div>
+        <BoxModalCompare session={session} />
+      </div>
+    </>
   );
 };
 export default ContentHomePage;
